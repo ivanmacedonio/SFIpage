73a85d2ca23653392f8c936c6aed7a37fc5de6c9
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-11-20 22:57:24 -0300

    fix: 100% width p1 p2 p3

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { Carrouselp2 } from "../components/Carrouselp2";
-import { Footer } from "../components/Footer";
 import { HeaderNormal } from "../components/Header-normal";
 export const Test = () => {
   return (
     <div>
       <div className="header">
         <HeaderNormal></HeaderNormal>
-      </div>
-      <div className="carrousel">
-        <Carrouselp2></Carrouselp2>
-      </div>
-      <div className="footer">
-        <Footer></Footer>
       </div>
     </div>
   );
