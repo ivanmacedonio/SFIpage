1f9a1acef596979f8a73635452816da7eb6da3c5
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-11-22 18:09:35 -0300

    fix: scroll fix

--- src/pages/Page2.jsx
+++ src/pages/Page2.jsx
@@ -14,6 +14,11 @@ export const Page2 = () => {
     "Construimos la confianza a través de la transparencia, la honestidad y la seguridad en todas nuestras interacciones. Nuestros socios confían en que estamos comprometidos a salvaguardar y hacer crecer su patrimonio de manera responsable y confiable. "
   );
 
+  var container1 = document.getElementById("set");
+  var container1Position = container1.getBoundingClientRect().top + window.scrollY - 330;
+
+
+
   return (
     <div className="page2" id="nosotros">
       <div className="header">
@@ -84,8 +89,8 @@ export const Page2 = () => {
                     );
                     setTitle("Confianza");
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -99,8 +104,8 @@ export const Page2 = () => {
                       "Operamos con los más altos estándares de seguridad en todos los aspectos de nuestro negocio. Desde la gestión de activos hasta la protección de los datos de nuestros socios, nos comprometemos a garantizar un entorno seguro y confiable. La seguridad de nuestras operaciones y la protección de nuestros clientes son fundamentales para nosotros.  "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -114,8 +119,8 @@ export const Page2 = () => {
                       "Estamos comprometidos con nuestros socios a largo plazo y trabajamos incansablemente para asegurarnos de que sus intereses siempre estén protegidos. Nuestra lealtad hacia nuestros socios se refleja en la construcción de relaciones sólidas y duraderas.  "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -129,8 +134,8 @@ export const Page2 = () => {
                       "Operamos de manera ética y moral, siempre tomando decisiones basadas en principios sólidos. Nos comprometemos a mantener los más altos estándares de integridad en todas nuestras actividades, brindando a nuestros socios la tranquilidad de saber que sus futuros prósperos están en buenas manos.  "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -148,8 +153,8 @@ export const Page2 = () => {
                       "Comunicamos de manera abierta y honesta con nuestros socios sobre su futuro. "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -163,8 +168,8 @@ export const Page2 = () => {
                       "Brindamos un servicio personalizado y atento a cada uno de nuestros socios. Brindando la plataforma para toda persona que quiera un mejor futuro."
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -178,8 +183,8 @@ export const Page2 = () => {
                       "Creemos en la justicia y la igualdad de oportunidades para todos nuestros socios, independientemente de la situación o circunstancias personales. Nos esforzamos por asegurarnos de que cada individuo tenga acceso a soluciones reales que les permitan alcanzar sus objetivos para futuro, de manera justa y equitativa. "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
@@ -193,8 +198,8 @@ export const Page2 = () => {
                       "Nos esforzamos por alcanzar la excelencia en la gestión empresarial, proporcionando un servicio al socio excepcional y comprometiéndonos a superar cualquier expectativa. Nuestro enfoque en la excelencia nos impulsa a ser líderes en la industria y a ofrecer a nuestros socios la máxima calidad en todas las áreas de nuestro negocio.  "
                     );
                     window.scrollTo({
-                      top: 1800,
-                      left: 150,
+                      top: container1Position,
+                      left: 0,
                       behavior: "smooth",
                     });
                   }}
