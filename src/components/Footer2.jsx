12ade60ffe15b3bce88fce2874c71b1cca3dc44b
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-11-22 15:40:09 -0300

    final commit: revision pending

---
+++ src/components/Footer2.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "../assets/logoSPI.png";
+import "../styles/Footer2.css";
+export const Footer2 = () => {
+  return (
+    <div className="footer2">
+      <img src={logo} alt="" />
+    </div>
+  );
+};
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { HeaderNormal } from "../components/Header-normal";
+import { Footer2 } from "../components/Footer2";
 export const Test = () => {
   return (
     <div>
       <div className="header">
-        <HeaderNormal></HeaderNormal>
+        <Footer2></Footer2>
       </div>
     </div>
   );
