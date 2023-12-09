242181d4a003801c70c9a5a3327d5eeae2e1f943
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-12-09 16:07:45 -0300

    checkpoint

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -12,44 +12,45 @@ export const Checkout = () => {
     const timer = setTimeout(() => {
       nav("/");
     }, 3500);
-    return () => clearTimeout(timer);
-  }, []);
 
-  async function Purchase() {
-    const token = localStorage.getItem("access");
-    if (token) {
-      const headers = {
-        Authorization: `Bearer ${token}`,
-      };
-      try {
-        const res2 = await axios.get(
-          "http://127.0.0.1:9000/api/get_user_selections/",
-          {
+    async function Purchase() {
+      const token = localStorage.getItem("access");
+      if (token) {
+        const headers = {
+          Authorization: `Bearer ${token}`,
+        };
+        try {
+          const res2 = await axios.get(
+            "http://127.0.0.1:9000/api/get_user_selections/",
+            {
+              headers: headers,
+            }
+          );
+          console.log(res2.data);
+          const res = await axios.post(`${BASE_URL}purchase/`, res2.data, {
             headers: headers,
-          }
-        );
-        console.log(res2.data);
-        const res = await axios.post(`${BASE_URL}purchase/`, res2.data, {
-          headers: headers,
-        });
-        console.log(res);
-      } catch (error) {
-        // nav('/')
-        console.log("error 1");
+          });
+          console.log(res);
+        } catch (error) {
+          // nav('/')
+          console.log("error 1");
+        }
+      } else {
+        // nav('/login')
+        console.log("error2");
       }
-    } else {
-      // nav('/login')
-      console.log("error2");
     }
-  }
+
+    if (canceled === "true") {
+      console.log("canceled");
+    } else {
+      Purchase();
+    }
+
+    return () => clearTimeout(timer);
+  }, [canceled]);
 
   const nav = useNavigate();
-
-  if (canceled === "true") {
-    console.log("canceled");
-  } else {
-    Purchase();
-  }
 
   return (
     <div>
