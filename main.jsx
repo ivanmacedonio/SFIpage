c1002f7a65fb76e74116676a8fed1a6e81488fdc
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-12-14 19:15:28 -0300

    chang route main

--- main.jsx
+++ main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom";
+import App from "./src/App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
