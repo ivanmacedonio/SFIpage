3eeef13fab900384cc029559b9a2ad2ace3de3ff
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-12-21 19:06:56 -0300

    change login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,16 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
-import chica from "../assets/afiliarse.webp";
-import { HeaderNormal } from "../components/Header-normal";
 import { BASE_URL } from "../hooks/fetch";
-
 import "../styles/Login.css";
 export const Login = () => {
   const [password, setPassword] = useState(false);
+  const [loading, setIsLoading] = useState(false);
   const { register, handleSubmit } = useForm({
     shouldUseNativeValidation: true,
   });
   const [error, setError] = useState("");
+
   const [display1, setDisplay1] = useState({
     display: "none",
   });
@@ -22,6 +21,7 @@ export const Login = () => {
   const nav = useNavigate();
 
   const onSubmit = async (data) => {
+    setIsLoading(true);
     try {
       // const res = await axios.post(`${BASE_URL_LOGIN}token/`, data);
       const res = await axios.post(`${BASE_URL}login/`, data);
@@ -35,9 +35,15 @@ export const Login = () => {
         headers: headers,
       });
       if (res2.data.KYC_Detail) {
-        nav("/");
+        setTimeout(() => {
+          setIsLoading(false);
+          nav("/");
+        }, 3000);
       } else {
-        nav("/verificacion");
+        setTimeout(() => {
+          setIsLoading(false);
+          nav("/verificacion");
+        }, 3000);
       }
     } catch (error) {
       setError("El usuario o la contraseña son invalidos");
@@ -46,43 +52,54 @@ export const Login = () => {
       });
     }
   };
+
   return (
     <div>
-      <div className="header">
-        <HeaderNormal></HeaderNormal>
-      </div>
-      <div className="logincontainer">
-        <img src={chica} alt="" />
-        <div className="formlogin">
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <h1>Iniciar sesión</h1>
-            <input
-              type="text"
-              placeholder="Ingrese el correo electronico"
-              {...register("username", { required: "Ingresa un email valido" })}
-            />
-            <input
-              type={password ? "text" : "password"}
-              placeholder="Ingrese la contraseña"
-              {...register("password", {
-                required: "Ingresa un password valido",
-              })}
-            />
-            <label>
-              <input type="checkbox" onClick={handleShow} />
-              Mostrar contraseña
-            </label>
-            <button type="submit">Enviar</button>
-            <div className="errorLogin" style={display1}>
-              <h3>{error}</h3>
-            </div>
-            <a href="" id="forgot">
-              Olvide mi contraseña
-            </a>
-            <Link to={"/register"}>
-              <div className="afiliar">No estas afiliado? Afiliate aquí</div>
-            </Link>
-          </form>
+      <div className="containergeneral">
+        <div className="logincontainer">
+          <h1 id="slogan">Smart Future Income</h1>
+          <h1>Inicia sesión en tu cuenta </h1>
+          <p>¡Nos alegramos de volver a verte!</p>
+          {/* <div className="tokens">
+            <button>Google</button>
+            <button>Facebook</button>
+          </div> */}
+
+          <div className="formlogin">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                type="text"
+                placeholder="Email"
+                {...register("username", {
+                  required: "Ingresa un email valido",
+                })}
+              />
+              <input
+                type={password ? "text" : "password"}
+                placeholder="Contraseña"
+                {...register("password", {
+                  required: "Ingresa un password valido",
+                })}
+              />
+              <label>
+                <input type="checkbox" onClick={handleShow} />
+                Mostrar contraseña
+              </label>
+              <button type="submit">
+                {loading ? <div class="spinner"></div> : "Ingresar"}
+              </button>
+
+              <div className="errorLogin" style={display1}>
+                <h3>{error}</h3>
+              </div>
+              <a href="" id="forgot">
+                Olvide mi contraseña
+              </a>
+              <Link to={"/register"}>
+                <div className="afiliar">¿No estas afiliado? Afiliate aquí</div>
+              </Link>
+            </form>
+          </div>
         </div>
       </div>
     </div>
