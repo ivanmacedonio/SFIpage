272e8506feceee615ff4ce31d8c02a74dee9403c
ivanmacedonio778 <ivanmacedonio778@gmail.com>
2023-12-23 11:59:59 -0300

    edit beneficiary

--- src/components/ModalProfile4.jsx
+++ src/components/ModalProfile4.jsx
@@ -3,7 +3,8 @@ import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
 import * as React from "react";
 import { useRef } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import PhoneInput from "react-phone-input-2";
 import "../styles/ModalProfile4.css";
 import BasicModal6 from "./ModalBeneficiario";
 import BasicModal5 from "./ModalProfile5";
@@ -26,6 +27,8 @@ export default function BasicModal4({ membershipData, wallet, activated }) {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [data, setData] = React.useState({});
+  const [edit, setEdit] = React.useState(false);
+  const [storage, setStorage] = React.useState();
   const [maxPer, setMaxPer] = React.useState(100);
   const [show, setShow] = React.useState(false);
   const [updatemembership, setUpdatemembership] = React.useState([]);
@@ -35,6 +38,8 @@ export default function BasicModal4({ membershipData, wallet, activated }) {
     register,
     handleSubmit,
     formState: { errors },
+    control,
+    setValue,
   } = useForm({
     shouldUseNativeValidation: true,
   });
@@ -59,13 +64,11 @@ export default function BasicModal4({ membershipData, wallet, activated }) {
       setShow(false);
       if (maxPer != 0) {
         setError("El porcentaje total de los beneficiarios debe ser del 100%");
-        
       }
       if (aditionalBeneficiaty.length === 0) {
         setError(
           "La lista de beneficiarios debe contar con al menos un miembro"
         );
-        
       }
     } else {
       setShow(true);
@@ -82,7 +85,20 @@ export default function BasicModal4({ membershipData, wallet, activated }) {
     setMaxPer(maxPer + num);
   }
 
+  function onSubmitEdit(data) {
+    setAditionalBeneficiaty((prevData) => [...prevData, data]);
+    const numper = parseFloat(data.percentage);
+    setMaxPer(maxPer - numper);
+    setEdit(!edit);
+  }
 
+  React.useEffect(() => {
+    setValue("full_name", storage?.full_name || "");
+    setValue("identification", storage?.identification || "");
+    setValue("email", storage?.email || "");
+    setValue("percentage", storage?.percentage || "");
+    setValue("phone", storage?.phone || "");
+  }, [setValue, storage]);
 
   return (
     <div>
@@ -95,63 +111,156 @@ export default function BasicModal4({ membershipData, wallet, activated }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style} id="scrolleable4">
+        <Box sx={style} id="scrollbenef">
           <h4
             onClick={() => {
-              handleClose();
+              {
+                edit ? setEdit(!edit) : handleClose();
+              }
             }}
             id="x"
-            style={{fontSize: '2rem'}}
+            style={{ fontSize: "2rem" }}
           >
             x
           </h4>
-          <h2 id="db">Datos de beneficiario</h2>
-
-          <div className="listaBeneficiarios">
-            {aditionalBeneficiaty.map((beneficiario) => (
-              <div className="box">
-                <label>
-                  <h3>{beneficiario.full_name}</h3>
-                  <h5>{beneficiario.percentage}%</h5>
-                </label>
-                <h3
-                  id="cruz"
-                  onClick={() => {
-                    handleDeleteBenef(beneficiario);
-                  }}
-                >
-                  X
-                </h3>
+          {edit ? (
+            <form onSubmit={handleSubmit(onSubmitEdit)}>
+              <div className="modal6">
+                <div className="caja4">
+                  <p>Nombre completo</p>
+                  <input
+                    type="text"
+                    placeholder="Nombre"
+                    {...register("full_name", {
+                      required: "Ingresa un nombre valido",
+                    })}
+                  />
+                </div>
+                <div className="caja4">
+                  <p>Identificación</p>
+                  <input
+                    type="text"
+                    placeholder="identification"
+                    {...register("identification", {
+                      required: "Ingresa una identificacion valida",
+                    })}
+                  />
+                </div>
+                <div className="caja4">
+                  <p>Teléfono</p>
+                  <Controller
+                    name="phone"
+                    control={control}
+                    rules={{ required: "Por favor ingresa tu numero" }}
+                    render={({ field }) => (
+                      <PhoneInput
+                        inputStyle={{ fontSize: "1.2rem", width: "100%" }}
+                        containerStyle={{ marginBottom: "1rem" }}
+                        type="number"
+                        placeholder="Número de celular"
+                        value={field.value}
+                        onChange={(value) => field.onChange(value)}
+                        className="no-spinner"
+                        country={"cr"}
+                      />
+                    )}
+                  />
+                </div>
+                <div className="caja4">
+                  <p>Correo electrónico</p>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    {...register("email", {
+                      required: "Ingresa un email valido",
+                    })}
+                  />
+                </div>
+                <div className="caja4">
+                  <p>Porcentaje</p>
+                  <input
+                    type="number"
+                    placeholder="Porcentaje"
+                    max={100}
+                    {...register("percentage", {
+                      required: "Ingresa un porcentaje valido",
+                    })}
+                  />
+                </div>
               </div>
-            ))}
-          </div>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="modal4">
-              <div className="hide">
-                <BasicModal6 onEnviarDatos={recibirBeneficiario}></BasicModal6>
-              </div>
-              <div className="continuar5">
-                {show ? (
-                  ""
-                ) : (
-                  <div className="errorbenef" ref={miRef}>
-                    <h2>{error}</h2>
+              <div className="continuar6">
+                <button type="submit">
+                  <div className="check2">
+                    <h2>Continuar</h2>
                   </div>
-                )}
-                {show && (
-                  <div className="tes">
-                    <BasicModal5
-                      membershipData={updatemembership}
-                      wallet={wallet}
-                      aditionalBeneficiaty={aditionalBeneficiaty}
-                      activated={activated}
-                    ></BasicModal5>
-                  </div>
-                )}
+                </button>
               </div>
+            </form>
+          ) : (
+            <div className="containeraux">
+              <h2 id="db">Datos de beneficiario</h2>
+
+              <div className="listaBeneficiarios">
+                {aditionalBeneficiaty.map((beneficiario) => (
+                  <div className="box">
+                    <label>
+                      <h3>{beneficiario.full_name}</h3>
+                      <h5>{beneficiario.percentage}%</h5>
+                    </label>
+                    <div className="optns">
+                      <h3
+                        id="cruz"
+                        onClick={() => {
+                          handleDeleteBenef(beneficiario);
+                        }}
+                      >
+                        X
+                      </h3>
+                      <h3
+                        id="btn-editar"
+                        onClick={() => {
+                          setEdit(!edit);
+                          setStorage(beneficiario);
+                          handleDeleteBenef(beneficiario);
+                        }}
+                      >
+                        Editar
+                      </h3>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="modal4">
+                  <div className="hide">
+                    <BasicModal6
+                      onEnviarDatos={recibirBeneficiario}
+                    ></BasicModal6>
+                  </div>
+                  <div className="continuar5">
+                    {show ? (
+                      ""
+                    ) : (
+                      <div className="errorbenef" ref={miRef}>
+                        <h2>{error}</h2>
+                      </div>
+                    )}
+                    {show && (
+                      <div className="tes">
+                        <BasicModal5
+                          membershipData={updatemembership}
+                          wallet={wallet}
+                          aditionalBeneficiaty={aditionalBeneficiaty}
+                          activated={activated}
+                        ></BasicModal5>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </form>
             </div>
-          </form>
+          )}
         </Box>
       </Modal>
     </div>
